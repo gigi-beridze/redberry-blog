a8fab9f3eeb366e4e2368c51f38de3a285bc13d4
gigiberidze <gigi.beridze.2018@gmail.com>
2023-12-24 00:58:06 +0400

    REDBERRY-08-refactor-filter-code

--- src/components/filters/index.tsx
+++ src/components/filters/index.tsx
@@ -1,39 +1,66 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Category } from "../../models/filters";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Category } from '../../models/filters';
 
-export const Filters = () => {
-    const [categories, setCategories] = useState<Category[]>([]);
-
-    useEffect(() => {
-        const url = 'https://api.blog.redberryinternship.ge/api/categories/',
-              token = 'af615548e1c85d4dc19c310b551d67dbdcd6db11d5e26961118f959931d50675',
-              config = {
-                headers: { Authorization: `Bearer ${token}` }
-              };
-  
-        axios.get(url, config)
-            .then(response => {
-                setCategories(response.data.data)
-            })
-            .catch(error => {
-                console.error('API Error:', error);
-            });
-    }, []); 
-    return (
-        <div className="filters">
-            {categories.map(item => (
-                <button    
-                    className="filter-btn"       
-                    style={{
-                        backgroundColor: item.background_color,
-                        color: item.text_color,
-                    }}
-                    key={item.id}
-                >
-                    {item.title}
-                </button>
-            ))}
-        </div>
-    )
+interface FiltersProps {
+  onSelectCategory: (category: string) => void;
 }
+
+const Filters: React.FC<FiltersProps> = ({ onSelectCategory }) => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  const fetchCategories = async () => {
+    try {
+      const url = 'https://api.blog.redberryinternship.ge/api/categories/';
+      const token = 'af615548e1c85d4dc19c310b551d67dbdcd6db11d5e26961118f959931d50675';
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+
+      const response = await axios.get(url, config);
+      setCategories(response.data.data);
+    } catch (error) {
+      console.error('API Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+    const storedCategories = localStorage.getItem('selectedCategories');
+    if (storedCategories) {
+      setSelectedCategories(JSON.parse(storedCategories));
+    }
+  }, []);
+
+  const handleCategoryClick = (category: string) => {
+    const updatedCategories = selectedCategories.includes(category)
+      ? selectedCategories.filter((c) => c !== category)
+      : [...selectedCategories, category];
+
+    setSelectedCategories(updatedCategories);
+    onSelectCategory(category);
+    localStorage.setItem('selectedCategories', JSON.stringify(updatedCategories));
+  };
+
+  const renderFilterButtons = () => {
+    return categories.map((item) => (
+      <button
+        className={`filter-btn ${selectedCategories.includes(item.title) ? 'selected' : ''}`}
+        style={{
+          backgroundColor: item.background_color,
+          color: item.text_color,
+          border: selectedCategories.includes(item.title) ? '1px solid black' : 'none',
+        }}
+        key={item.id}
+        onClick={() => handleCategoryClick(item.title)}
+      >
+        {item.title}
+      </button>
+    ));
+  };
+
+  return <div className="filters">{renderFilterButtons()}</div>;
+};
+
+export default Filters;
